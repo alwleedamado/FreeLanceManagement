8fc78aae6b055791cf9df538eead729ae0cc9ee5
Alwaleed Ahmed <alwleedamado@gmail.com>
2022-11-02 21:49:34 +0200

    Add theme components

--- freelanceManagmentClient/src/app/theme/theme.module.ts
+++ freelanceManagmentClient/src/app/theme/theme.module.ts
@@ -1,12 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { routing } from './routing';
+import { CoreModule } from '../core/core.module';
+import { ThemeComponent } from './theme/theme.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AsideComponent } from './aside/aside.component';
+import { AsideMenuComponent } from './aside-menu/aside-menu.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: ThemeComponent,
+children: routing
+  }
+]
 @NgModule({
-  declarations: [],
+  declarations: [
+    ThemeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AsideComponent,
+    AsideMenuComponent,
+    BreadcrumbComponent
+  ],
   imports: [
-    CommonModule
+    CoreModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class ThemeModule { }
