762d1c5c6b9bd8afd3c37f4164cdb9f2a19c80f2
Alwaleed Ahmed <alwleedamado@gmail.com>
2022-11-08 18:16:16 +0200

    UI state ngrx effects

--- freelanceManagmentClient/src/app/core/reducers/index.ts
+++ freelanceManagmentClient/src/app/core/reducers/index.ts
@@ -2,11 +2,12 @@
 import { routerReducer } from '@ngrx/router-store';
 import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { errorsReducer } from './error-state/error-state.reducer';
+import { uiReducer } from './ui-state/ui-state.reducer';
 
 
 // tslint:disable-next-line:no-empty-interface
 export interface AppState { }
 
-export const reducers: ActionReducerMap<AppState> = {  router: routerReducer, errorState: errorsReducer };
+export const reducers: ActionReducerMap<AppState> = { ui: uiReducer, router: routerReducer, errorState: errorsReducer };
 
 export const metaReducers: MetaReducer<AppState>[] = []
